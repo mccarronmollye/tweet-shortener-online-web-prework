554c49dcaf9eb7330775646a1f426069cad55e61
Molly McCarron <mccarronmollye@gmail.com>
2018-11-15 23:58:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,6 +41,11 @@ end
 def shortened_tweet_truncator(tweet)
   if tweet.length > 140
     word_substituter(tweet)
+      if word_substituter(tweet) > 140
+        tweet.truncate(140)
+      else
+        word_substituter(tweet)
+      end
   else
     tweet
   end
