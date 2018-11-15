fe2215f4b61a8afad7532a53f64c5f83626a1e68
Molly McCarron <mccarronmollye@gmail.com>
2018-11-15 23:06:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+dictionary = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+
+def word_substituter(tweet)
+  tweet.split.collect do |word|
+  if dictionary.keys.include?(word.downcase)
+    word = dictionary[word.downcase]
+  else
+    word
+  end
+end.join(" ")  
+end
